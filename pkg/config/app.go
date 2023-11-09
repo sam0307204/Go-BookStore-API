7123cf3f054bc7a2f668236535ba7e7d5b7af166
Dhanush <119780513+sam0307204@users.noreply.github.com>
2023-11-09 07:13:49 +0530

    Update app.go

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:&ifconfig2309!_69*@(127.0.0.1:3306)/library?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "<username>:<password>@(127.0.0.1:3306)/library?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
